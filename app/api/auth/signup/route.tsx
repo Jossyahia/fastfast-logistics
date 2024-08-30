baebabaae60cd1c7e91fe2a12ecb9b8cf5e39101
Ahia <jossyahia@gmail.com>
2024-08-30 11:12:20 +0100

    fix well

--- app/api/auth/signup/route.tsx
+++ app/api/auth/signup/route.tsx
@@ -8,7 +8,11 @@ const signupSchema = z.object({
   name: z.string().min(2, "Name must be at least 2 characters long"),
   email: z.string().email("Invalid email address"),
   password: z.string().min(8, "Password must be at least 8 characters long"),
-  role: z.enum([Role.RIDER, Role.USER], "Invalid role"),
+  role: z
+    .enum([Role.RIDER, Role.USER])
+    .refine((val) => Object.values(Role).includes(val), {
+      message: "Invalid role",
+    }),
 });
 
 export async function POST(req: Request) {
--- app/rider/dashboard/page.tsx
+++ app/rider/dashboard/page.tsx
@@ -1,7 +1,6 @@
-// app/rider/dashboard/page.tsx
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
-import prisma  from "@/lib/prisma";
+import prisma from "@/lib/prisma";
 import BookingList from "@/components/BookingList";
 import { configureAbly } from "@/lib/ably";
 import { Booking } from "@prisma/client";
@@ -10,7 +9,7 @@ export default async function RiderDashboard() {
   const session = await auth();
 
   if (!session || session.user.role !== "RIDER") {
-    console.log(session)
+    console.log(session);
     redirect("/api/auth/signin");
   }
 
@@ -31,6 +30,12 @@ export default async function RiderDashboard() {
   }
 
   const ablyKey = process.env.ABLY_API_KEY;
+
+  if (!ablyKey) {
+    console.error("Ably API key is not defined");
+    // Handle the missing API key, perhaps by showing an error message
+    return <div>Error: Unable to initialize real-time updates</div>;
+  }
 
   return (
     <div className="container mx-auto px-4 py-8">
