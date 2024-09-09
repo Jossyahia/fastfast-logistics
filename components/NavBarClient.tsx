88aa7f0dbf5d78cfc58b92806b39f91c5fd4db55
Ahia <jossyahia@gmail.com>
2024-09-09 15:42:32 +0100

    404 page

--- components/NavBarClient.tsx
+++ components/NavBarClient.tsx
@@ -211,7 +211,7 @@ const NavBarClient: React.FC = () => {
                     )}
                   </Button>
                   {isProfileMenuOpen && (
-                    <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-neutral-100 dark:bg-neutral-900 transition-colors duration-200">
                       <Link
                         href="/profile"
                         className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
